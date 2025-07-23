1da5f5bd166aa3eea3fc5a26336693dc610db18d
phawdk <NOX6DEHVV5@proton.me>
2025-07-23 14:47:22 +0000

    Delete src directory for update

--- src/worker.ts
+++
@@ -1,3 +0,0 @@
-// Everything imported here will be bundeled into the worker. I know it seems a bit pointless right now
-import { modifyPDFWithComments } from "./render-pdf";
-export { modifyPDFWithComments };
